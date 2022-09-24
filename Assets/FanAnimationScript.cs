66f65618916b79f8646a336b8c213f7fca98628f
iceisak <icemannen.lindgren@gmail.com>
2022-09-24 15:00:23 +0200

    Fixat nya assets
    Fixat nya assets och sprites på bana 3

--- Assets/FanAnimationScript.cs
+++ Assets/FanAnimationScript.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class FanAnimationScript : MonoBehaviour
 {
-    public GameObject airPushing; 
-
+    public GameObject airPushing;
+    public bool Active = true;
+    private int BinaryActivation = 1;
+    public bool TimerActivated = false;
+    private float timer;
+    public float duration = 5;
+    public bool TimerBased = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +20,12 @@ public class FanAnimationScript : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
-    }
+    {if (TimerBased == true)
+        {
+            Timer();
+            FanState();
+        }
+     }
 
 
     void FanPause()
@@ -28,6 +37,40 @@ public class FanAnimationScript : MonoBehaviour
     {
         gameObject.GetComponent<Animator>().enabled = true;
         airPushing.SetActive(true);
+    }
+    void Timer()
+    {
+        timer += Time.deltaTime;
+        if (timer >= duration)
+        {
+            timer = 0f;
+            BinaryActivation *= -1;
+
+        }
+    }
+
+    private bool BinaryReader()
+    {
+        if (BinaryActivation > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void FanState()
+    {
+        if (BinaryReader() == true)
+        {
+            FanContinue();
+        }
+        else
+        {
+            FanPause();
+        }
     }
 
 }
